d082c452035fe9970f3583aa40e44cd66d0ebe91
joseflores1 <joseflores@alumnos.uai.cl>
2025-05-08 10:56:25 -0400

    Add functionality to write initial .json file if not found

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,57 +4,63 @@ import (
 	"encoding/json"
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 const (
-	JSON_NAME = ".gatorconfig.json"
+	JSON_NAME    = ".gatorconfig.json"
+	INITIAL_JSON = `{"db_url": "postgres://example"}`
 )
 
 type Config struct {
-	DB_URL            string `json:"db_url"`
-	Current_user_name string `json:"current_user_name"`
+	DBURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
 }
 
 // Returns ~/JSON_NAME filepath
 func getConfigFilePath() (string, error) {
+
 	// Get HOME dir
 	homeDir, errDir := os.UserHomeDir()
 	if errDir != nil {
 		return "", fmt.Errorf("error when trying to get HOME dir: %w", errDir)
 	}
 
-	return homeDir + "/" + JSON_NAME, nil
+	// Construct full path (OS-agnostic)
+	fullPath := filepath.Join(homeDir, JSON_NAME)
+
+	return fullPath, nil
 }
 
-func write(cfg Config) error {
-	configPath, errConfigPath := getConfigFilePath()
-	if errConfigPath != nil {
-		return fmt.Errorf("error when trying to get config file path: %w", errConfigPath)
-	}
-	configData, errMarshal := json.Marshal(cfg)
-	if errMarshal != nil {
-		return fmt.Errorf("error when trying to marshal Config struct: %w", errMarshal)
-	}
-	errWrite := os.WriteFile(configPath, configData, 0777)
-	if errWrite != nil {
-		return fmt.Errorf("error when trying to write to config file: %w", errWrite)
-	}
-
-	return nil
-}
-
-// Reads config file and returns it into Config struct
+// Reads config file and returns it into a Config struct
 func Read() (Config, error) {
+
 	// Get HOME dir
 	configPath, errConfigPath := getConfigFilePath()
-	if errConfigPath!= nil {
+	if errConfigPath != nil {
 		return Config{}, fmt.Errorf("error when trying to get config file path: %w", errConfigPath)
 	}
-	// Read ~/JSON_NAME file
-	fileData, errRead := os.ReadFile(configPath)
-	if errRead != nil {
-		return Config{}, fmt.Errorf("error when trying to read JSON file: %w", errRead)
+
+	// Read ~/JSON_NAME file, writes it if not found
+	_, errRead := os.ReadFile(configPath)
+
+	if errRead != nil && errRead.Error() == fmt.Sprintf("open %s: no such file or directory", configPath) {
+
+		fmt.Println(".gatorconfig.json not found, writing it to HOME dir")
+
+		errWrite := os.WriteFile(configPath, []byte(INITIAL_JSON), 0666)
+		if errWrite != nil {
+			return Config{}, fmt.Errorf("error when trying to write initial JSON: %w", errWrite)
+		}
+
 	}
+
+	// Read ~/JSON_NAME file again and return if error
+	fileData, errReadTwo := os.ReadFile(configPath)
+	if errReadTwo != nil {
+		return Config{}, fmt.Errorf("error when trying to read initial JSON file: %w", errReadTwo)
+	}
+
 	// Unmarshal read file
 	var readConfig Config
 	errUnmarshal := json.Unmarshal(fileData, &readConfig)
@@ -65,13 +71,35 @@ func Read() (Config, error) {
 	return readConfig, nil
 }
 
+// Sets username and writes Config struct to disk
 func (c *Config) SetUser(currentUserName string) error {
+
 	// Set config field
-	c.Current_user_name = currentUserName
+	c.CurrentUserName = currentUserName
+
 	// Write to config file
 	errWrite := write(*c)
 	if errWrite != nil {
 		return fmt.Errorf("error when trying to set user: %w", errWrite)
+	}
+
+	return nil
+}
+
+// Writes the Config struct to ~/JSON_NAME filepath
+func write(cfg Config) error {
+
+	configPath, errConfigPath := getConfigFilePath()
+	if errConfigPath != nil {
+		return fmt.Errorf("error when trying to get config file path: %w", errConfigPath)
+	}
+	configData, errMarshal := json.Marshal(cfg)
+	if errMarshal != nil {
+		return fmt.Errorf("error when trying to marshal Config struct: %w", errMarshal)
+	}
+	errWrite := os.WriteFile(configPath, configData, 0666)
+	if errWrite != nil {
+		return fmt.Errorf("error when trying to write to config file: %w", errWrite)
 	}
 
 	return nil
--- main.go
+++ main.go
@@ -2,28 +2,36 @@ package main
 
 import (
 	"fmt"
+	"log"
+
 	"github.com/joseflores1/rss/internal/config"
 )
 
 const (
 	CURRENT_USER = "josei"
 )
+
 func main() {
 	// Read file and get Config struct
 	configStruct, errRead := config.Read()
 	if errRead != nil {
-		fmt.Println(errRead)
+		log.Fatal(errRead)
 	}
-	// Set username of Config struct and write it to disk	
+
+	fmt.Printf("The contents of the initial config struct are: %+v\n", configStruct)
+
+	// Set username of Config struct and write it to disk
 	errSetUser := configStruct.SetUser(CURRENT_USER)
 	if errSetUser != nil {
-		fmt.Println(errSetUser)
+		log.Fatal(errSetUser)
 	}
+
 	// Read updated file and get new Config struct
 	newConfigStruct, errNewRead := config.Read()
 	if errNewRead != nil {
-		fmt.Println(errNewRead)
+		log.Fatal(errNewRead)
 	}
+
 	// Print config struct's contents
 	fmt.Printf("The contents of the config struct are: %+v\n", newConfigStruct)
 }
