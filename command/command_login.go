c730902bd3caef05534f247baaf90969bd108187
joseflores1 <joseflores@alumnos.uai.cl>
2025-05-08 11:42:04 -0400

    Add command package for CLI

---
+++ command/command_login.go
@@ -0,0 +1,23 @@
+package command
+
+import (
+	"fmt"
+)
+
+func handlerLogin(s *state, cmd command) error {
+
+	if len(cmd.arguments) != 1 {
+
+		return fmt.Errorf("error: gator login <username> expects only an <username> argument")
+	}
+
+	userName := cmd.arguments[0]
+	errSetUser := s.config.SetUser(userName)
+	if errSetUser != nil {
+		return fmt.Errorf("error when trying to set username in login handler: %w", errSetUser)
+	}
+
+	fmt.Printf("The %s username has been set by the login handler!\n", userName)
+
+	return nil
+}
